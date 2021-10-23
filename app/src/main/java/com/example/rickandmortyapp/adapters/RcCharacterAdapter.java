3163c905459d4e18db00dda8b345e401b44fe50a
Vikas.Saini <vikas.saini@letstrack.in>
2021-10-23 20:24:17 +0530

    updated

--- app/src/main/java/com/example/rickandmortyapp/adapters/RcCharacterAdapter.java
+++ app/src/main/java/com/example/rickandmortyapp/adapters/RcCharacterAdapter.java
@@ -1,27 +1,16 @@
 package com.example.rickandmortyapp.adapters;
 
 import android.content.Context;
-import android.graphics.Typeface;
-import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.GlideException;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.target.Target;
-import com.example.rickandmortyapp.Model.ApiResponse;
 import com.example.rickandmortyapp.Model.Characters;
 import com.example.rickandmortyapp.R;
 import com.example.rickandmortyapp.databinding.CharactersRowBinding;
-import com.example.rickandmortyapp.interfaces.RecyclerViewClickListener;
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
@@ -55,6 +44,7 @@ public class RcCharacterAdapter extends RecyclerView.Adapter<RcCharacterAdapter.
         holder.binding.tvNameCh.setText(dataList.get(position).getChname());
         holder.binding.tvstatus.setText(dataList.get(position).getStatus());
         holder.binding.tvspecies.setText(dataList.get(position).getSpecies());
+        holder.binding.tvGender.setText(dataList.get(position).getGender());
         holder.binding.tvlocation.setText(dataList.get(position).getLocation().getLocationname());
         Picasso.get()
                 .load(dataList.get(position).getImageurl())
